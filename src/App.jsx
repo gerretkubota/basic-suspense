c47c96468ccb2de82bf1f31d54026a3465adabc1
Gerret Mozii <gerretkubota@gmail.com>
2022-05-12 21:55:22 -0700

    adding agents and maps components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Agents from './Features/Agents';
+import Maps from './Features/Maps';
 
 function App() {
   return (
     <div className="App">
       <header>Valorant</header>
       <Agents />
+      <Maps />
     </div>
   );
 }
