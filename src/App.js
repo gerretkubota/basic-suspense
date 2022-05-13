16e8949ff4f1aeb080d742dac1daa3faafa77fb6
Gerret Mozii <gerretkubota@gmail.com>
2022-05-12 21:35:02 -0700

    setting up eslint

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        my header
-      </header>
+      <header className="App-header">my header</header>
+      <p>
+        lskfjlksjdfklsjdfs;kdf;sdf;lsdkfsdkjssdfkjsdkfljsfljsdfldfjdsffjsfsdfkjsdlkfjdlfkdsjflksdflsjlfjskslfjslfsklfjsdlkfjdsfkldsfj
+      </p>
     </div>
   );
 }
