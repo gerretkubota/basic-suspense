2a0841eea4e8f0bd1e05d14fa4d0d0dcc6a7251a
Gerret Mozii <gerretkubota@gmail.com>
2022-05-12 21:51:18 -0700

    fixed eslint issue

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Agents from './Features/Agents';
+
+function App() {
+  return (
+    <div className="App">
+      <header>Valorant</header>
+      <Agents />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Features/Agents/Agents.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { createUseStyles } from 'react-jss';
+
+function Agents() {
+  return <div>Agents</div>;
+}
+
+export default Agents;
